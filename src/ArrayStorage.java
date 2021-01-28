76ab28aa0afc2fef42b918eddc38e8d15610487b
Ostalf <ostalfgladyre@gmail.com>
2021-01-28 14:30:31 +0700

    Added methods to class ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.bcel.internal.generic.ATHROW;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,68 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i <size() ; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; ) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            } else {
+                i++;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid)) {
+                return resume;
+            } else {
+                return new Resume();
+            }
+        }
+        return new Resume();
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                if (i == 0){
+                    for (int j = i; j < size(); j++) {
+                        storage[j--] = storage[j];
+                    }
+                    break;
+                }
+                for (int j = i; j < size(); j++) {
+                    storage[j--] = storage[j];
+                }
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allResume = new Resume[size()];
+        for (int i = 0; i < size(); i++) {
+            allResume[i] =  storage[i];
+        }
+        return allResume;
     }
 
     int size() {
+        for (int i = 0; i < storage.length; i++ ) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
         return 0;
     }
 }
